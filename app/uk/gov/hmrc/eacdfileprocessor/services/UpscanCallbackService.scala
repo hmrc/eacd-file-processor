3be81ea0ab444c4c0201d1705404f9eb744429d9
dwalters <32767569+david-walters-HMRC@users.noreply.github.com>
2026-05-22 10:59:25 +0100

    GLSD-19736 added additional logging

--- app/uk/gov/hmrc/eacdfileprocessor/services/UpscanCallbackService.scala
+++ app/uk/gov/hmrc/eacdfileprocessor/services/UpscanCallbackService.scala
@@ -16,6 +16,7 @@
 
 package uk.gov.hmrc.eacdfileprocessor.services
 
+import play.api.Logging
 import play.api.mvc.Request
 import uk.gov.hmrc.eacdfileprocessor.connectors.EmailConnector
 import uk.gov.hmrc.eacdfileprocessor.models.{AuditEvents, CallbackBody, Details, FailedCallbackBody, ReadyCallbackBody}
@@ -28,7 +29,7 @@ import scala.concurrent.{ExecutionContext, Future}
 @Singleton
 class UpscanCallbackService @Inject()(sessionStorage: UploadProgressTracker,
                                       val auditConnector: AuditConnector,
-                                      emailConnector: EmailConnector) extends AuditEvents {
+                                      emailConnector: EmailConnector) extends AuditEvents with Logging {
   private val allowedMimeTypes: Set[String] = Set(
     "text/csv" // .csv
   )
@@ -44,12 +45,13 @@ class UpscanCallbackService @Inject()(sessionStorage: UploadProgressTracker,
           checksum = s.uploadDetails.checksum
         ))
       case f: FailedCallbackBody =>
-        for {
+        (for {
           uploadDetails <- sessionStorage.getUploadResult(callback.reference).map {
             case Some(details) => details
             case None => throw new RuntimeException("Upload details not found for reference: " + callback.reference)
           }
-          _ <- auditConnector.sendExtendedEvent(
+          _ <-
+            auditConnector.sendExtendedEvent(
             EmailEvent(
               fileReference = uploadDetails.reference.value,
               requestorId = uploadDetails.requestorPID,
@@ -60,7 +62,8 @@ class UpscanCallbackService @Inject()(sessionStorage: UploadProgressTracker,
               hc = hc
             )
           )
-          _ <- emailConnector.sendEmail(
+          _ <-
+            emailConnector.sendEmail(
             requestorName = uploadDetails.requestorName,
             fileName = uploadDetails.details.map {
               case Details.UploadedSuccessfully(name, _, _, _, _) => name
@@ -73,7 +76,9 @@ class UpscanCallbackService @Inject()(sessionStorage: UploadProgressTracker,
             failureMessage = f.failureDetails.message,
             templateId = "emac_helpdesk_bulk_deenrolment_file_upload_failure"
           )
-        } yield ()
+        } yield ()).recover {
+          case ex => logger.error(s"issue occurred when sending email and audit ${ex.getMessage}")
+        }
 
         Some(Details.UploadedFailed(
           failureReason = f.failureDetails.failureReason,
