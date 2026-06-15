852193f9dec5cc98c79083e7fb797ab8de4660de
Rob Petterson <rob.petterson1@digital.hmrc.gov.uk>
2026-06-15 13:21:08 +0100

    Remove duplicate and unused imports in DeEnrolmentWorkItemRepository

--- it/test/uk/gov/hmrc/eacdfileprocessor/repository/DeEnrolmentWorkItemRepositoryISpec.scala
+++ it/test/uk/gov/hmrc/eacdfileprocessor/repository/DeEnrolmentWorkItemRepositoryISpec.scala
@@ -17,7 +17,6 @@
 package uk.gov.hmrc.eacdfileprocessor.repository
 
 import helper.IntegrationSpec
-import org.mongodb.scala.SingleObservableFuture
 import org.mongodb.scala.model.{Filters, Updates}
 import org.scalatest.matchers.should.Matchers.shouldBe
 import play.api.test.Helpers
