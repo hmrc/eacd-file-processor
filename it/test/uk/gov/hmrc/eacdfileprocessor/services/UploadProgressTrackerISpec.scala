25b0b6938a3c5cbc8434dce94abda51efd252ac1
finlaypugh07 <254463720+finlaypugh07@users.noreply.github.com>
2026-06-29 14:59:09 +0100

    GLSD-20910:It test fix

--- it/test/uk/gov/hmrc/eacdfileprocessor/services/UploadProgressTrackerISpec.scala
+++ it/test/uk/gov/hmrc/eacdfileprocessor/services/UploadProgressTrackerISpec.scala
@@ -112,14 +112,12 @@ class UploadProgressTrackerISpec extends IntegrationSpec with TestData with Even
           owner = any[String]
         )(using any[HeaderCarrier])
       ).thenReturn(Future.failed(new TimeoutException("Unable to upload, time out.")))
-      when(progressTracker.transferToObjectStore(sucessfulDetails.downloadUrl, sucessfulDetails.mimeType, sucessfulDetails.checksum, sucessfulDetails.name, reference)).thenReturn(Future.unit)
 
-      val file = await(fileRepository.findByReference(reference)).get
-      file.status mustBe INITIAL
-
-      for {
-        _ <- progressTracker.registerUploadResult(reference, sucessfulDetails)
+      val result = await(for {
+        _              <- progressTracker.registerUploadResult(reference, sucessfulDetails)
         uploadedResult <- fileRepository.findByReference(reference)
-      } yield uploadedResult.get.status mustBe SCANNED
+      } yield uploadedResult)
+
+      result.get.status mustBe SCANNED
     }
   }
