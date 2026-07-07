603cad7477de045c94057512693c07b30b5f7b04
CPardeep <75028018+CPardeep@users.noreply.github.com>
2026-07-07 13:31:57 +0100

    GLSD-20059: addressed comments

--- app/uk/gov/hmrc/eacdfileprocessor/repository/FileRecordValidationErrorRepository.scala
+++ app/uk/gov/hmrc/eacdfileprocessor/repository/FileRecordValidationErrorRepository.scala
@@ -18,13 +18,12 @@ package uk.gov.hmrc.eacdfileprocessor.repository
 
 import org.bson.types.ObjectId
 import org.mongodb.scala.model.{Filters, Sorts}
-import play.api.libs.functional.syntax.{toFunctionalBuilderOps, toInvariantFunctorOps}
 import play.api.libs.functional.syntax.toFunctionalBuilderOps
 import play.api.libs.json.*
 import uk.gov.hmrc.eacdfileprocessor.models.{FileRecordValidationError, Reference}
 import uk.gov.hmrc.mongo.MongoComponent
-import uk.gov.hmrc.mongo.play.json.formats.{MongoFormats, MongoJavatimeFormats}
 import uk.gov.hmrc.mongo.play.json.PlayMongoRepository
+import uk.gov.hmrc.mongo.play.json.formats.{MongoFormats, MongoJavatimeFormats}
 
 import java.time.Instant
 import javax.inject.{Inject, Singleton}
@@ -33,6 +32,7 @@ import scala.concurrent.{ExecutionContext, Future}
 object FileRecordValidationErrorFormats {
 
   private given Format[ObjectId] = MongoFormats.objectIdFormat
+
   private given Format[Instant] = MongoJavatimeFormats.instantFormat
 
   val fileRecordValidationErrorFormat: Format[FileRecordValidationError] =
