af46d79c787987471596dfd7645c8454f8be1c6f
danny-waughman <daniel.waughman@digital.hmrc.gov.uk>
2026-08-21 11:06:16 +0100

    Fixing test compilation warnings

--- test/uk/gov/hmrc/eacdfileprocessor/controllers/TestControllerSpec.scala
+++ test/uk/gov/hmrc/eacdfileprocessor/controllers/TestControllerSpec.scala
@@ -24,7 +24,7 @@ import play.api.http.Status.CREATED
 import play.api.mvc.*
 import uk.gov.hmrc.objectstore.client.{ObjectSummaryWithMd5, Path}
 import uk.gov.hmrc.objectstore.client.Md5Hash
-import play.api.test.Helpers.{DELETE, GET, INTERNAL_SERVER_ERROR, NO_CONTENT, OK, POST, contentAsString, status}
+import play.api.test.Helpers.{DELETE, INTERNAL_SERVER_ERROR, OK, POST, contentAsString, status}
 import play.api.test.{DefaultAwaitTimeout, FakeRequest, Helpers}
 import uk.gov.hmrc.eacdfileprocessor.helper.{TestData, TestSupport}
 import uk.gov.hmrc.eacdfileprocessor.models.auth.AuthRequest
@@ -33,7 +33,6 @@ import uk.gov.hmrc.eacdfileprocessor.testOnly.controllers.TestController
 import uk.gov.hmrc.http.{Authorization, HeaderCarrier}
 import uk.gov.hmrc.internalauth.client.{BackendAuthComponents, Predicate, Retrieval}
 import uk.gov.hmrc.objectstore.client.play.PlayObjectStoreClient
-import uk.gov.hmrc.objectstore.client.{Md5Hash, ObjectSummaryWithMd5, Path}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
